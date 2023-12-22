0bc22fc35b349bec2cc09e87fadde1d2ccbdbad0
luke <142288087+lukemawuntu@users.noreply.github.com>
2023-12-22 21:44:08 +0800

    Add game() to contain previous functions

--- script.js
+++ script.js
@@ -3,37 +3,53 @@ function getComputerChoice(){
   return choice[Math.floor(Math.random()*3)]
 }
 
+let winCounter = 0;
+let loseCounter = 0;
 function playRound(playerSelection, computerSelection){
   if (playerSelection=="rock" && computerSelection=="rock"){
     return "Tie";
   }
   else if (playerSelection=="rock" && computerSelection=="paper"){
-    return "You lose";
+    return loseCounter++;
   }
   else if (playerSelection=="rock" && computerSelection=="scissor"){
-    return "You win";
+    return winCounter++;
   }
   else if (playerSelection=="paper" && computerSelection=="rock"){
-    return "You win";
+    return winCounter++;
   }
   else if (playerSelection=="paper" && computerSelection=="paper"){
     return "Tie";
   }
   else if (playerSelection=="paper" && computerSelection=="scissor"){
-    return "You lose";
+    return loseCounter++;
   }
   else if (playerSelection=="scissor" && computerSelection=="rock"){
-    return "You lose";
+    return loseCounter++;
   }
   else if (playerSelection=="scissor" && computerSelection=="paper"){
-    return "You win";
+    return winCounter++;
   }
-  else if (playerSelection=="scissor" && computerSelection=="scissor"){
+   else if (playerSelection=="scissor" && computerSelection=="scissor"){
     return "Tie";
   }
 }
 
-let playerSelection = prompt("Choose rock paper scissor").toLowerCase();
-let computerSelection = getComputerChoice();
+function game(){
+  let playerSelection = prompt("Choose rock paper scissor").toLowerCase();
+  let computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection)
+  console.log(`You = ${playerSelection} || Computer = ${computerSelection}`)
+  console.log(`Win = ${winCounter} || Lose = ${loseCounter}`)
+}
 
-console.log(playRound(playerSelection,computerSelection))
+while (true){
+  game();
+  if (winCounter==3 || loseCounter==3){
+    break;
+  }
+}
+
+(winCounter>loseCounter)
+? console.log("You win!")
+: console.log("You lose.")
